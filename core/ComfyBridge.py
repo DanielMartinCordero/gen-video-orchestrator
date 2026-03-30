751cb78c8e5840df4a832785e625367b9e85c46e
Daniel Martin <danikai1902@gmail.com>
2026-03-31 00:39:04 +0200

    Refactor comments and fix typo in ComfyBridge

--- core/ComfyBridge.py
+++ core/ComfyBridge.py
@@ -1,5 +1,5 @@
-import requests # Librería externa para HTTP
-import json     # Librería nativa (no necesita instalación)
+import requests 
+import json 
 
 class ComfyBridge:
     def __init__(self, server_url="http://127.0.0.1:8188"):
@@ -9,27 +9,27 @@ class ComfyBridge:
     def generate_image(self, workflow_data, prompt_text, id_prompt, id_latent):
         """With the prompt, ask Comfy Ui to generate a picture or video"""
 
-        # Clonamos el diccionario (Deep Copy) para evitar efectos secundarios
+        # Diccionary clone
         actual_workflow = json.loads(json.dumps(workflow_data))
 
-        # Inyectamos valores
+        # Embed values
         actual_workflow[id_prompt]["inputs"]["text"] = prompt_text
         actual_workflow[id_latent]["inputs"]["width"] = 512
         actual_workflow[id_latent]["inputs"]["height"] = 912
 
         payload = {
             "prompt": actual_workflow,
-            "client_id": "creator_ai_orchestrator" # Corregido typo: orchestrator
+            "client_id": "creator_ai_orchestrator" 
         }
 
         try:
-            # Usamos la variable correcta: self.server_url
+           
             response = requests.post(f"{self.server_url}/prompt", json=payload)
             response.raise_for_status()
 
-            # Devolvemos el ID de la tarea para poder rastrearla luego
+            # return id
             return response.json().get('prompt_id')
 
         except Exception as e:
-            print(f"❌ [ComfyBridge] Error al conectar: {e}")
+            print(f"[ComfyBridge] Error al conectar: {e}")
             return None
